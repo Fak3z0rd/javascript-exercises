d11303f703b87874cabe323e92e73b061f4837e7
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-07 12:37:52 -0300

    exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    newArray = [...array];
 
+    for (j = 1; j < arguments.length; j++) {
+        for (let i = 0; i < newArray.length; i++) {
+            if (newArray[i] === arguments[j]) {
+                newArray.splice(i, 1);
+            }
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
