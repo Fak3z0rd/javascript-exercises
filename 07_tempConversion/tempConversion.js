f3758f64afc91d8032ecd8eac9c739eb34270628
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-07 13:39:20 -0300

    exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ctof = function (temperature) {
+    fahrenheit = temperature * (9 / 5) + 32;
+    return parseFloat(fahrenheit.toFixed(1));
 };
 
-const ctof = function() {
-
+const ftoc = function (temperature) {
+    celsius = ((temperature - 32) * 5) / 9;
+    return parseFloat(celsius.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
