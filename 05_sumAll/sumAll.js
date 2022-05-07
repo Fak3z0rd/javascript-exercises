3ea33d806738e54d51c7fce7b69fa8ceb5899a7b
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-07 12:54:49 -0300

    exexrcise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    if (n1 < 0 || n2 < 0 || Number.isInteger(n1) == false || Number.isInteger(n2) == false) {
+        return "ERROR";
+    }
 
+    let min = n1;
+    let max = n2;
+    if (n1 > n2) {
+        max = n1;
+        min = n2;
+    }
+
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
