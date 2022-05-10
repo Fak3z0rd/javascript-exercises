ebef1f9d768b9297911cf2442b69afdd76589292
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-10 10:23:25 -0300

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (number <= 0) {
+        return "OOPS";
+    }
+    let n1 = 0;
+    let n2 = 1;
+    let nth = 0;
+    let i = 1;
+    let x = [n2];
+    while (i < number) {
+        nth = n1 + n2;
+        x.push(nth);
+        n1 = n2;
+        n2 = nth;
+        i++;
+    }
+    return x[number - 1];
 };
 
 // Do not edit below this line
