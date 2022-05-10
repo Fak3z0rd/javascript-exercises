6525ae0c256875d7e6f162a31d0a3588e43b9a74
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-10 09:53:28 -0300

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let re = /[^A-Za-z0-9]/g;
+    let lowRegString = str.toLowerCase().replace(re, "");
+    let reverseString = lowRegString.split("").reverse().join("");
 
+    return lowRegString === reverseString;
 };
 
 // Do not edit below this line
