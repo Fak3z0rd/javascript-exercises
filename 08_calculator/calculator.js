55df3d469d19589116fb0784d533c8c9d8025983
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-10 09:07:24 -0300

    exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,66 @@
-const add = function() {
-	
+const add = function () {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        if (Number.isNaN(arguments[i])) {
+            return "Only numbers allowed";
+        }
+        sum += arguments[i];
+    }
+    return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+    let sub = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        if (Number.isNaN(arguments[i])) {
+            return "Only numbers allowed";
+        }
+        sub -= arguments[i];
+    }
+    return sub;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    let arr = [...array];
+    let sum = arr.reduce(function (total, num) {
+        return total + num;
+    }, 0);
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    let arr = [...array];
+    let mult = arr.reduce(function (total, num) {
+        return total * num;
+    }, 1);
+    return mult;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+    return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+    if (number === 0) {
+        return 1;
+    }
+    let arr = [];
+    for (let i = 1; i <= number; i++) {
+        arr.push(i);
+    }
+
+    return arr.reduce(function (total, num) {
+        return total * num;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
