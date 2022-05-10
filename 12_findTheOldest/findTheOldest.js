5a75195362a9fe5c50029afad7cc717a2e408cec
root <root@DESKTOP-IUBQCDI.localdomain>
2022-05-10 10:57:34 -0300

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce(function (prevPerson, currentPerson) {
+        const oldestAge = getAge(prevPerson.yearOfBirth, prevPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? prevPerson : currentPerson;
+    });
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
